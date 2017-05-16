70cbc28bd4f14f1c865dda9e20f296ac9093b1ec
Daniel Deutsch <deudan1010@gmail.com>
2017-05-16 23:36:07 +0200

    add bodyparser package and require it in app

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const jsonParser = require('body-parser').json;
 
 const app = express();
+
+app.use(jsonParser());
+
+app.use((req, res, next) => {
+  console.log(`Testing middleware ${req.query.times} times!`);
+  next();
+});
 
 const port = process.env.port || 3000;
 

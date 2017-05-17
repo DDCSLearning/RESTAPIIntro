3c6492ca34f821c8c739fb7375654a2addd219d7
Daniel Deutsch <deudan1010@gmail.com>
2017-05-17 17:38:17 +0200

    connect to mongodb, add mongoose package, update watch script

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const app = express();
 
 const jsonParser = require('body-parser').json;
 const logger = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/QA');
+const db = mongoose.connection;
+
+db.on('error', err => {
+  console.error(`Error while connecting to DB: ${err.message}`);
+});
+db.once('open', () => {
+  console.log('DB connected successfully!');
+});
 
 app.use(logger('dev'));
 app.use(jsonParser());

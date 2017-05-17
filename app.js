8d17823056924a96f8ca350fbba636ac0b5680c7
Daniel Deutsch <deudan1010@gmail.com>
2017-05-17 15:55:07 +0200

    adding the morgan package to the app, create all answer routes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const jsonParser = require('body-parser').json;
 const routes = require('./routes');
-
 const app = express();
 
+const jsonParser = require('body-parser').json;
+const logger = require('morgan');
+
+app.use(logger('dev'));
 app.use(jsonParser());
 app.use('/questions', routes);
-
 
 const port = process.env.port || 3000;
 
--- routes.js
+++ routes.js
@@ -1,16 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
+// question routes
 router.get('/', (req, res) => {
-  res.json({ response: 'a GET request' });
+  res.json({ response: 'a GET request for LOOKING at questions' });
 });
 
 router.post('/', (req, res) => {
-  res.json({ response: 'a POST request', body: req.body });
+  res.json({
+    response: 'a POST request for CREATING questions',
+    body: req.body
+  });
 });
 
-router.get('/:id', (req, res) => {
-  res.json({ response: `a GET request for special id: ${req.params.id}`});
+router.get('/:qID', (req, res) => {
+  res.json({
+    response: `a GET request for LOOKING at a special answer id: ${req.params.qID}`
+  });
+});
+
+// answer routes
+router.post('/:qID/answers', (req, res) => {
+  res.json({
+    response: 'a POST request for CREATING answers',
+    question: req.params.qID,
+    body: req.body
+  });
+});
+
+router.put('/:qID/answers/:aID', (req, res) => {
+  res.json({
+    response: 'a PUT request for EDITING answers',
+    question: req.params.qID,
+    answer: req.params.aID,
+    body: req.body
+  });
+});
+
+router.delete('/:qID/answers/:aID', (req, res) => {
+  res.json({
+    response: 'a DELETE request for DELETING answers',
+    question: req.params.qID,
+    answer: req.params.aID,
+    body: req.body
+  });
+});
+
+router.post('/:qID/answers/:aID/vote-:dec', (req, res) => {
+  res.json({
+    response: 'a POST request for VOTING on answers',
+    question: req.params.qID,
+    answer: req.params.aID,
+    vote: req.params.dec,
+    body: req.body
+  });
 });
 
 module.exports = router;

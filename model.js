f474a8746d7547f24f176b0b77140c3fc5ce89c2
Daniel Deutsch <deudan1010@gmail.com>
2017-05-17 20:54:49 +0200

    add prehook sorting and updating newest answer and votes with methods

--- model.js
+++ model.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
+
+const sortAnswers = (a, b) => {
+  if (a.votes === b.votes) {
+    return b.updatedAt - a.updatedAt;
+  }
+  return b.votes - a.votes;
+};
 
 const AnswerSchema = new Schema({
   text: String,
@@ -8,10 +14,30 @@ const AnswerSchema = new Schema({
   updatedAt: { type: Date, default: Date.now },
   votes: { type: Number, default: 0 }
 });
+
+AnswerSchema.method('update', (updates, callback) => {
+  Object.assign(this, updates, { updatedAt: new Date() });
+  this.parent().save(callback);
+});
+
+AnswerSchema.method('vote', (vote, callback) => {
+  if (vote === 'up') {
+    this.votes += 1;
+  } else {
+    this.votes -= 1;
+  }
+  this.parent().save(callback);
+});
+
 const QuestionSchema = new Schema({
   text: String,
   createdAt: { type: Date, default: Date.now },
   answers: [AnswerSchema]
+});
+
+QuestionSchema.pre('save', next => {
+  this.answers.sort(sortAnswers);
+  next();
 });
 
 const Question = mongoose.model('Question', QuestionSchema);
